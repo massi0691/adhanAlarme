b4e09773aa47bdcaeacaca457436156610bfad93
massi0691 <massi9106.ait@gmail.com>
2026-09-13 08:47:22 +0200

    Initial Commit

---
+++ Adhan-Alarme/Adhan_AlarmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adhan_AlarmeApp.swift
+//  Adhan-Alarme
+//
+//  Created by massi9106 on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Adhan_AlarmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adhan-AlarmeTests/Adhan_AlarmeTests.swift
@@ -0,0 +1,19 @@
+//
+//  Adhan_AlarmeTests.swift
+//  Adhan-AlarmeTests
+//
+//  Created by massi9106 on 13/09/2026.
+//
+
+import Testing
+@testable import Adhan_Alarme
+
+struct Adhan_AlarmeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
